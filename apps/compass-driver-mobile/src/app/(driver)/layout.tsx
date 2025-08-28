c35836e605b6a1e39b08d955bdbe05fd38a81ea0
Aitegin <jyldyzbekov06aiti@gmail.com>
2025-08-28 17:07:40 +0600

    fix:layout driver footer for Iphones

--- apps/compass-driver-mobile/src/app/(driver)/layout.tsx
+++ apps/compass-driver-mobile/src/app/(driver)/layout.tsx
@@ -22,9 +22,9 @@ export default async function DriverLayout({ children }: DriverLayoutProps) {
       <NotificationProvider>
         <NotificationsProvider>
           <LocationProvider intervalMs={30000}>
-            <div className='flex flex-col h-screen ios-fix'>
+            <div className='flex flex-col h-screen'>
               <DriverMobileHeader />
-              <main className='flex-1 overflow-y-auto pb-20 safe-area-bottom ios-main'>{children}</main>
+              <main className='flex-1 overflow-y-auto pb-20'>{children}</main>
               <DriverMobileFooter />
 
               {/* Модальное окно для входящих заказов - теперь самодостаточное */}
